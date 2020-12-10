75c0c3f70e71f0b2bacbd0f375065f65a95371e3
PensActius <37923848+pensactius@users.noreply.github.com>
2020-12-10 12:42:52 +0100

    Add files via upload

---
+++ ESP32/Bluetooth/BlueTooth2LCD_basico/Bluetooth2LCD_basico.ino
@@ -0,0 +1,35 @@
+#include <BluetoothSerial.h>
+#include <Wire.h>
+#include <hd44780.h>
+#include <hd44780ioClass/hd44780_I2Cexp.h>
+
+BluetoothSerial bt;
+hd44780_I2Cexp lcd;
+
+void setup() {
+  Serial.begin(115200);
+  bt.begin("ESP32 WROOM");
+  Serial.print("Device ready");
+
+  int status;
+  status = lcd.begin(16, 2);
+  if (status != 0)
+  {
+    hd44780::fatalError(status);    // Ya no vuelve, se queda aquí
+  }
+  lcd.clear();
+}
+
+void loop() {
+  if (Serial.available()) {
+    bt.write(Serial.read());
+  }
+
+  if (bt.available()) {
+    char c = bt.read();
+    if (c > 31) {
+      lcd.write (c);
+    }
+  }
+  delay(20);
+}
